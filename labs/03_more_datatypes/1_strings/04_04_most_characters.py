6f8611433eab03fb114f105611e767fd007d495d
cocoonkid <frederik.hossak@tiersigma.com>
2019-02-19 22:04:14 +0800

    Second Push of Python Prep-Labs

--- labs/03_more_datatypes/1_strings/04_04_most_characters.py
+++ labs/03_more_datatypes/1_strings/04_04_most_characters.py
@@ -5,11 +5,21 @@ Write a script that takes three strings from the user and prints the one with th
 
 
 
-text1 = input("Please give me a text: ")
+text1=input("Please give me a text: ")
+text2=input("Please give me a text: ")
+text3=input("Please give me a text: ")
+num1 = len(text1)
+num2 = len(text2)
+num3 = len(text3)
 
-text2 = input("Please give me a text: ")
+if num1 > num2 and num1 > num3
+    print(num1)
+elsif num2 > num1 and num2 > num3
+    print(num1)
+elsif num3 > num1 and num3 > num2
+else
+    print("There is no longest text.")
 
-text3 = input("Please give me a text: ")
 
 
 
